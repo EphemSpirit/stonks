e800fa561e2944bc3b47c1693baab8075014f0f1
Drew Hund <languageinvestigator@gmail.com>
2020-12-05 16:45:02 -0500

    added stock tracking

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.find_price(ticker)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex[:publishable_token],
                                   secret_token: Rails.application.credentials.iex[:secret_token],
@@ -9,6 +14,10 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker)
+    where(ticker: ticker).first
   end
 
 end
